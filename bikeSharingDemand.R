0f21f1e9fb9ceb539b305136418fb5d94192c070
Wijnand Nuij <wijnandnuij@gmail.com>
2014-06-15 21:57:38 +0200

    add randomforest

--- bikeSharingDemand.R
+++ bikeSharingDemand.R
@@ -7,13 +7,17 @@ runTestSet <- function(targetLocation='/home/wijnand/R_workspace_bikedemand/reso
         
         trainDataWeek <- trainData[trainData$weekday=="1" | trainData$weekday=="2"| trainData$weekday=="3" | trainData$weekday=="4" 
                                | trainData$weekday=="5",]
+        #trainDataWeek <- trainDataWeek[1:3000,]
         
         trainDataWeekend <- trainData[trainData$weekday=="6" | trainData$weekday=="7",]
         trainDataWeekend$holiday <- NULL
         
         print(str(trainDataWeek))
-        m5treeModelWeek <- m5Tree(trainDataWeek[-1])
-        m5treeModelWeekend <- m5Tree(trainDataWeekend[-1])
+        require(randomForest)
+        weekModel <- randomForestTree(trainDataWeek[-1])
+        print('trained weekmodel')
+        weekendModel <- randomForestTree(trainDataWeekend[-1])
+        print('trained weekendmodel')
         
         testData <- loadData('/home/wijnand/R_workspace_bikedemand/resources/test.csv')
         testData <- addDateVariables(testData)
@@ -21,31 +25,34 @@ runTestSet <- function(targetLocation='/home/wijnand/R_workspace_bikedemand/reso
                                                    | testData$weekday=="5",]
         testDataWeekend <- testData[testData$weekday=="6" | testData$weekday=="7",]
         
-        result_m5week <- predict(m5treeModelWeek, testDataWeek)
-        result_m5weekend <- predict(m5treeModelWeekend, testDataWeekend)
+        result_week <- predict(weekModel, testDataWeek)
+        print('predicted week')
+        result_weekend <- predict(weekendModel, testDataWeekend)
+        print('predicted weekend')
         
-        result_m5week <- ifelse(result_m5week < 1, 1, result_m5week)
-        result_m5weekend <- ifelse(result_m5weekend < 1, 1, result_m5weekend)
+        result_week <- ifelse(result_week < 1, 1, result_week)
+        result_weekend <- ifelse(result_weekend < 1, 1, result_weekend)
         
         result <- NULL
         result$datetime <- append(testDataWeek$datetime, testDataWeekend$datetime)
-        result$count <- c(result_m5week, result_m5weekend)
+        result$count <- c(result_week, result_weekend)
         write.csv(result, targetLocation, quote=F, row.names=F)
 }
-
-
 
 main <- function(weekend=FALSE)
 {
         trainData <- loadData()
         trainData <- addDateVariables(trainData)
         
-        if(weekend)
+        if(weekend==FALSE)
         {
                 trainData <- trainData[trainData$weekday=="1" | trainData$weekday=="2"| trainData$weekday=="3" | trainData$weekday=="4" 
                                         | trainData$weekday=="5",]
+                trainData$holiday <- NULL
+                
+                trainData <- trainData[1:3000,]
         }
-        else
+        else if(weekend==TRUE)
         {
                 trainData <- trainData[trainData$weekday=="6" | trainData$weekday=="7",]
                 trainData$holiday <- NULL
@@ -55,19 +62,25 @@ main <- function(weekend=FALSE)
         m5treeModel <- m5Tree(trainData[-1])
         regTree <- regTree(trainData[-1])
         linearModel <- linearModel(trainData[-1])
+        randomForestModel <- randomForestTree(trainData[-1])
         
         result_m5 <- predict(m5treeModel, trainData[-1])
         result_regTree <- predict(regTree, trainData[-1])
         result_linearModel <- predict(linearModel, trainData[-1])
+        result_randomForest <- predict(randomForestModel, trainData[-1])
         
         # values < 0 are not allowed
         result_m5 <- ifelse(result_m5 < 1, 1, result_m5)
         result_regTree <- ifelse(result_regTree < 1, 1, result_regTree)
         result_linearModel <- ifelse(result_linearModel < 1, 1, result_linearModel)
+        result_randomForest <- ifelse(result_randomForest < 1, 1, result_randomForest)
+        print(summary(result_randomForest))
+        print(summary(trainData$count))
         
         print(paste0('m5tree correlation:              ', cor(result_m5, trainData$count)))
         print(paste0('regression tree correlation:     ', cor(result_regTree, trainData$count)))
         print(paste0('linearModel correlation:         ', cor(result_linearModel, trainData$count)))
+        print(paste0('randomForstModel correlation:    ', cor(result_randomForest, trainData$count)))
 }
 
 addDateVariables <- function(data)
@@ -89,6 +102,13 @@ addDateVariables <- function(data)
         data
 }
 
+randomForestTree <- function(trainData)
+{
+        require(randomForest)
+        trainedModel <- randomForest(count ~ . , trainData, ntree=100)
+        #print(summary(trainedModel))
+        trainedModel
+}
 
 m5Tree <- function(trainData)
 {
